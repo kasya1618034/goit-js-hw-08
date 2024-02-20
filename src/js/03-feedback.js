9a44c4075b9bfbcd25ba7adb07c7921d0499edd6
Katarzyna Filas <katarzyna.filas@gmail.com>
2024-02-20 20:14:14 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,5 @@
+import throttle from 'lodash.throttle';
 
+const email = document.querySelector('input[name="email"]').value.trim();
+const message = document.querySelector('textarea[name="message"]').value.trim();
+const formData = { email, message };
