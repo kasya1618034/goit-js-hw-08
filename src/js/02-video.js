76d525bad9e6df8b05233cbc2528cf33b8e7b009
Katarzyna Filas <katarzyna.filas@gmail.com>
2024-02-20 19:00:24 +0100

    Add library lodash.throttle and update task02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,11 @@
 import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 
-player.on('timeupdate', function() {
-    console.log('played the video!');
-});
+const videoCurrentTime = throttle(data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000);
+
+player.on('timeupdate', videoCurrentTime);
