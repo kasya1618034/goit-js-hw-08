b010ace8fd177c2c69c4976d835da782bb516240
Katarzyna Filas <katarzyna.filas@gmail.com>
2024-02-20 17:38:45 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,8 @@
 import Vimeo from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+player.on('play', function() {
+    console.log('played the video!');
+});
